88596953ec3edf841b9d07fc6aa3e6c4b8d1ef81
Namrouud <slitikhalil1998@gmail.com>
2019-08-07 12:21:26 +0200

    first commit

---
+++ src/List.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Listitem from "./Listitem"
+function List ()
+{
+
+
+    return (<ul><Listitem/></ul>);
+}
+export default List;
---
+++ src/Listitem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Listitem ()
+{
+
+
+    return (
+    <li>khalil</li>
+    );
+}
+export default Listitem;
